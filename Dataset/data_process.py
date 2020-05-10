4d881b580bbc4057efcfb226391939562d01de18
jiashuaizhang <henu_zjs@163.com>
2020-05-10 14:59:58 +0800

    filter data and normalize style

--- Dataset/data_process.py
+++ Dataset/data_process.py
@@ -8,5 +8,32 @@
 # Date:         2020/5/10
 #-------------------------------------------------------------------------------
 
+from Util import FileUtil
+
+def line2upper(input_file, output_file):
+
+    lines = FileUtil.readFile2List(input_file)
+    new_lines = [line.upper() for line in lines]
+    FileUtil.writeList2File(new_lines, output_file)
+
+
+
 if __name__ == '__main__':
+
+    # 将disease-miRNA中的数据都改写为大写格式
+    # input_file1 = "./disease-miRNA.txt"
+    # output_file1 = "./disease-miRNA2.txt"
+    # line2upper(input_file1, output_file1)
+
+    # 将disease-pathway中的数据都改写为大写格式
+    # input_file1 = "./disease-pathway.txt"
+    # output_file1 = "./disease-pathway2.txt"
+    # line2upper(input_file1, output_file1)
+
+    # 将disease-lncRNA中的数据都改写为大写格式
+    input_file1 = "./disease-lncRNA.txt"
+    output_file1 = "./disease-lncRNA2.txt"
+    line2upper(input_file1, output_file1)
+
+
     pass
